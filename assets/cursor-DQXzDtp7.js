e5b8aed5216246a6d6171251a5f1c0878b645b38
farmils <farmilsonelove@mail.ri>
2026-06-23 20:33:51 +0300

    Updates

---
+++ assets/cursor-DQXzDtp7.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-erk6KA30.js";import"./index-BbGzxop8.js";import"./index.es-CKjDWZE3.js";import"./fallback-0M8LrmP2.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
