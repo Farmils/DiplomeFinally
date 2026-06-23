022d90adf07b9ffcbb6a0c99c781a45dd5548f8f
farmils <farmilsonelove@mail.ri>
2026-06-23 18:12:01 +0300

    Updates

---
+++ assets/cursor-DmeN9Gmu.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-BhdQbBYD.js";import"./index-Cwh7gqs2.js";import"./index.es-Bk0RZwKt.js";import"./fallback-K4CC15Ra.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
